b943e98756286accf5b07b2035c8042e58b359b2
hojoonji <sleeperbus@gmail.com>
2016-05-25 08:17:03 +0900

    ..

--- chapter03/cluster.py
+++ chapter03/cluster.py
@@ -2,6 +2,7 @@
 
 from math import sqrt
 from PIL import Image, ImageDraw
+import random
 
 # 파일을 읽고 잘라서 반환한다. 
 def readfile(filename):
@@ -167,6 +168,155 @@ def drawnode(draw, clust, x, y, scaling, labels):
   # leaf node 라 blog name 을 출력한다. 
   else:
     draw.text((x+5, y-7), labels[clust.id], (0, 0, 0))
+
+
+# 매트릭스를 회전시킨다. 
+def rotatematrix(data):
+  newdata = []
+  for i in range(len(data[0])):
+    newrow = [data[j][i] for j in range(len(data))]
+    newdata.append(newrow)
+  return newdata
+
+################################################################################
+# kmeans algorithm
+################################################################################
+def kcluster(rows, distance=pearson, k=4):
+  # 개별 column vector 의 (min, max) 값을 구해서 전체 컬럼의 minmax 리스트를 만든다. 
+  ranges = [(min(row[i] for row in rows), max(row[i] for row in rows)) 
+  for i in range(len(rows[0]))]
+
+  # k 개의 임의 벡터를 만든다. 
+  clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+  for i in range(len(rows[0]))] for j in range(k)]
+
+  lastmatches = None 
+  # 반복작업 횟수 설정
+  for t in range(100):
+    print 'Iteration %d' % t
+    # 각 cluster 에 할당되는 vector 을 저장할 리스트 
+    bestmatches = [[] for i in range(k)]
+
+    # 각 row 를 k 개의 클러스터에 할당한다. 
+    for j in range(len(rows)):
+      row = rows[j]
+      bestmatch = 0
+      for i in range(k):
+        d = distance(clusters[i], row)
+        if d < distance(clusters[bestmatch], row): bestmatch = i
+      bestmatches[bestmatch].append(j)
+
+    # bestmatches 가 이전 loop 의 결과와 같다면 빠져나간다. 
+    if bestmatches == lastmatches: break
+    lastmatches = bestmatches
+
+    # 각 클러스터의 중심을 구한다. 
+    for i in range(k):
+      avgs = [0.0] * len(rows[0])
+      if len(bestmatches[i]) > 0:
+        for rowid in bestmatches[i]:
+          # 각 컬럼의 값을 누적한다. 
+          for m in range(len(rows[rowid])):
+            avgs[m] += rows[rowid][m]
+          # 각 컬럼을 평균을 구한다.
+        for j in range(len(avgs)):
+          avgs[j] /= len(bestmatches[i])
+        clusters[i] = avgs
+
+  return bestmatches
+
+
+# tanamoto 계수 
+def tanamoto(v1, v2):
+  c1, c2, shr = 0, 0, 0
+  for i in range(len(v1)):
+    if v1[i] != 0: c1 += 1
+    if v2[i] != 0: c2 += 1
+    if v1[i] != 0 and v2[i] != 0: shr += 1
+
+  return 1.0 - (float(shr) / (c1 + c2 - shr))
+
+################################################################################
+# data 를 2차원 좌표로 나타낸 묶음을 반환한다. 
+def scaledown(data, distance=pearson, rate=0.01):
+  n = len(data)
+
+  # 각 vector 의 실제거리를 구한다. 
+  realdist = [[distance(data[i], data[j]) for j in range(n)] for i in range(0, n)]
+
+  outersum = 0.0
+
+  # 2차원 좌표 위의 임의의 n개 점을 설정한다. 
+  loc = [[random.random(), random.random()] for i in range(n)]
+  # 2차원 위 각 점 사이의 거리를 저장할 변수 
+  dist2d = [[0.0 for j in range(n)] for i in range(n)]
+
+  lasterror = None
+  for m in range(0, 1000):
+    # 각 점 사이의 거리를 구합시다. 
+    for i in range(n):
+      for j in range(n):
+        # 동일한 점 사이의 거리는 구할 필요가 없다. 어차피 0
+        if i == j: continue
+        dist2d[i][j] = sqrt(sum([pow(loc[i][x] - loc[j][x], 2) for x in range(len(loc[i]))]))
+
+    # 각 점의 위치를 보정할 값을 설정한다. 
+    grad = [[0.0, 0.0] for i in range(n)]
+
+    totalerror = 0
+    for i in range(n):
+      for j in range(n):
+        if i == j: continue
+        # 거리 매트릭스는 대각선을 기준으로 대칭이므로 (i, j) 나 (j, i)나 동일하다. 
+        # 책에서는 굳이 왜 (j, i) 라고 썼을까?
+
+        # 2차원거리와 실제거리의 차이를 비율을 구한다. 거리가 비슷하다면 0에 가까울 것이고 
+        # 차이가 난다면 값이 커진다. 
+        errorterm = (dist2d[j][i] - realdist[j][i])/realdist[j][i]
+
+        # i점의 x, y 위치를 보정할 계수. j와의 차이 값들을 누적한다.  
+        # dist2d 와 나누는 이유는? 내 생각으로는, 너무 휙휙 많이 움직이지 않기 위해서... 
+        # dist2d 로 나누게 되면 현재의 i, j을 삼각형으로 한 cos, sin 값이 나오는데 
+        # 이 값은 0과 1 사이이다. 
+        grad[i][0] += ((loc[i][0] - loc[j][0])/dist2d[j][i]) * errorterm
+        grad[i][1] += ((loc[i][1] - loc[j][1])/dist2d[j][i]) * errorterm
+
+        # print "(%d, %d) = %f" % (i, j, errorterm)
+        totalerror += abs(errorterm)
+    print "%dth : %f" % (m, totalerror)
+
+    if lasterror and lasterror < totalerror: break
+    lasterror = totalerror
+
+    # 점들의 위치를 이동한다. 
+    for k in range(n):
+      loc[k][0] -= rate * grad[k][0]
+      loc[k][1] -= rate * grad[k][1]
+
+  return loc
+
+# 2D 로 표현된 데이터
+def draw2d(data, labels, jpeg='mds2d.jpg'):
+  img = Image.new('RGB', (2000, 2000), (255, 255, 255))
+  draw = ImageDraw.Draw(img)
+
+  for i in range(len(data)):
+    x = (data[i][0] + 0.5) * 1000
+    y = (data[i][1] + 0.5) * 1000
+    draw.text((x, y), labels[i], (0, 0, 0))
+  img.save(jpeg, 'JPEG')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
