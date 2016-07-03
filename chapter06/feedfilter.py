7c6f3500a3887cb43f8d47c6d74cc44d9d933e10
Hojoon Ji <sleeperbus@gmail.com>
2016-07-03 23:27:06 +0000

    ...

--- chapter06/feedfilter.py
+++ chapter06/feedfilter.py
@@ -14,18 +14,18 @@ def read(feed, classifier):
     
     fulltext = '%s\n%s\n%s' % (entry['title'], entry['publisher'], entry['summary'])
     
-    print 'Guess: ' + str(classifier.classify(fulltext))
+    print 'Guess: ' + str(classifier.classify(entry))
     
     cl = raw_input('Enter category: ')
-    classifier.train(fulltext, cl)
+    classifier.train(entry, cl)
                 
-def entryfetures(entry):
+def entryfeatures(entry):
   splitter = re.compile('\\W*')
   f = {}
 
   # 제목에는 특정 태그를 달고 단어별로 분리한다. 
   titlewords = [s.lower() for s in splitter.split(entry['title']) 
-   if len(s> 2 and len(s) < 20)]
+                 if len(s) > 2 and len(s) < 20]
   for w in titlewords: f['Title:'+w] = 1
   
   # 요약에서 단어를 추출함 
