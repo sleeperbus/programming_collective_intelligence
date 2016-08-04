be6e921e10d946dc71b687310a34184b3d21687b
hojoonji <sleeperbus@gmail.com>
2016-08-04 14:52:46 +0900

    from others

--- chapter08/numpredict.py
+++ chapter08/numpredict.py
@@ -1,138 +1,212 @@
-# -*- coding: utf-8 -*-
-from random import random, randint
+from random import random,randint
 import math
 
-weightdomain = [(0, 20)] * 4
-
-# 가격모델
-def wineprice(rating, age):
-  peak_age = rating-50
-
-  price = rating/2
-  if age > peak_age:
-    # 최적나이에서 5년이 지나버리면 0으로 수렴
-    price = price*(5-(age-peak_age))
+def wineprice(rating,age):
+  peak_age=rating-50
+  
+  # Calculate price based on rating
+  price=rating/2
+  if age>peak_age:
+    # Past its peak, goes bad in 10 years
+    price=price*(5-(age-peak_age)/2)
   else:
-    # 최적나이에 가까워질수록 최고 5배 가격까지 상승
-    price = price*(5*((age+1)/peak_age))
-
-  if price < 0: price = 0
+    # Increases to 5x original value as it
+    # approaches its peak
+    price=price*(5*((age+1)/peak_age))
+  if price<0: price=0
   return price
 
 
-# 와인데이터 셋 생성
 def wineset1():
-  rows = []
+  rows=[]
   for i in range(300):
-    rating = random()*50+50
-    age = random()*50
+    # Create a random age and rating
+    rating=random()*50+50
+    age=random()*50
+
+    # Get reference price
+    price=wineprice(rating,age)
     
-    # 정확한 와인의 가격
-    price = wineprice(rating, age)
-    # 그 가격에 노이즈를 추가
-    price *= (random()*0.4+0.8)
-    rows.append({'input':(rating, age), 'result':price})
+    # Add some noise
+    price*=(random()*0.2+0.9)
+
+    # Add to the dataset
+    rows.append({'input':(rating,age),
+                 'result':price})
   return rows
 
-# 와인데이터 셋 생성
-def wineset2():
-  rows = []
-  for i in range(300):
-    rating = random()*50+50
-    age = random()*50
-    aisle = float(randint(1, 20))
-    bottlesize= [375.0, 750.0, 1500.0, 3000.0][randint(0,3)]
-    
-    # 정확한 와인의 가격
-    price = wineprice(rating, age)
-    price *= (bottlesize/750)
-    # 그 가격에 노이즈를 추가
-    price *= (random()*0.4+0.8)
-    rows.append({'input':(rating, age, aisle, bottlesize), 'result':price})
-  return rows
-
-# 거리계산 함수
-def euclidean(v1, v2):
-  d = 0.0
+def euclidean(v1,v2):
+  d=0.0
   for i in range(len(v1)):
-    d += (v1[i]-v2[i])**2
+    d+=(v1[i]-v2[i])**2
   return math.sqrt(d)
 
-def getdistances(data, vec1):
-  distancelist = []
+
+def getdistances(data,vec1):
+  distancelist=[]
+  
+  # Loop over every item in the dataset
   for i in range(len(data)):
-    vec2 = data[i]['input']
-    distancelist.append((euclidean(vec1, vec2), i))
+    vec2=data[i]['input']
+    
+    # Add the distance and the index
+    distancelist.append((euclidean(vec1,vec2),i))
+  
+  # Sort by distance
   distancelist.sort()
   return distancelist
 
-def knnestimate(data, vec1, k=3):
-  dlist = getdistances(data, vec1)
-  avg = 0.0
-
+def knnestimate(data,vec1,k=5):
+  # Get sorted distances
+  dlist=getdistances(data,vec1)
+  avg=0.0
+  
+  # Take the average of the top k results
   for i in range(k):
-    idx = dlist[i][1]
-    avg += data[idx]['result']
-  avg = avg/k
+    idx=dlist[i][1]
+    avg+=data[idx]['result']
+  avg=avg/k
   return avg
 
-def weightedknn(data, vec1, k=5, weightf=gaussian):
-  dlist = getdistances(data, vec1)
-  avg = 0.0
-  totalweight = 0.0
-
-  for i in range(k):
-    dist = dlist[i][0]
-    idx = dlist[i][1]
-    weight = weightf(dist)
-    avg += weight*data[idx]['result']
-    totalweight += weight
-  avg = avg/totalweight
-  return avg
-
-def inverseweight(dist, num=1.0, const=0.1):
+def inverseweight(dist,num=1.0,const=0.1):
   return num/(dist+const)
 
-def subtractweight(dist, const=1.0):
-  if dist > const: return 0
-  else: return const-dist
+def subtractweight(dist,const=1.0):
+  if dist>const: 
+    return 0
+  else: 
+    return const-dist
 
-def gaussian(dist, sigma=10.0):
+def gaussian(dist,sigma=5.0):
   return math.e**(-dist**2/(2*sigma**2))
 
+def weightedknn(data,vec1,k=5,weightf=gaussian):
+  # Get distances
+  dlist=getdistances(data,vec1)
+  avg=0.0
+  totalweight=0.0
+  
+  # Get weighted average
+  for i in range(k):
+    dist=dlist[i][0]
+    idx=dlist[i][1]
+    weight=weightf(dist)
+    avg+=weight*data[idx]['result']
+    totalweight+=weight
+  if totalweight==0: return 0
+  avg=avg/totalweight
+  return avg
 
-def dividedata(data, test=0.05):
-  trainset = []
-  testset = []
+def dividedata(data,test=0.05):
+  trainset=[]
+  testset=[]
   for row in data:
-    if random() < test: testset.append(row)
-    else: trainset.append(row)
-  return trainset, testset
+    if random()<test:
+      testset.append(row)
+    else:
+      trainset.append(row)
+  return trainset,testset
 
-def testalgorithm(algf, trainset, testset):
-  error = 0.0
+def testalgorithm(algf,trainset,testset):
+  error=0.0
   for row in testset:
-    guess = algf(trainset, row['input'])
-    error += (row['result']-guess)**2
+    guess=algf(trainset,row['input'])
+    error+=(row['result']-guess)**2
+    #print row['result'],guess
+  #print error/len(testset)
   return error/len(testset)
 
-def crossvalidate(algf, data, trials=100, test=0.05):
-  error = 0.0
+def crossvalidate(algf,data,trials=100,test=0.1):
+  error=0.0
   for i in range(trials):
-    trainset, testset = dividedata(data, test)
-    error += testalgorithm(algf, trainset, testset)
+    trainset,testset=dividedata(data,test)
+    error+=testalgorithm(algf,trainset,testset)
   return error/trials
 
-def rescale(data, scale):
-  scaleddata = []
+def wineset2():
+  rows=[]
+  for i in range(300):
+    rating=random()*50+50
+    age=random()*50
+    aisle=float(randint(1,20))
+    bottlesize=[375.0,750.0,1500.0][randint(0,2)]
+    price=wineprice(rating,age)
+    price*=(bottlesize/750)
+    price*=(random()*0.2+0.9)
+    rows.append({'input':(rating,age,aisle,bottlesize),
+                 'result':price})
+  return rows
+
+def rescale(data,scale):
+  scaleddata=[]
   for row in data:
-    scaled = [scale[i]*row['input'][i] for i in range(len(scale))]
-    scaleddata.append({'input': scaled, 'result': row['result']})
+    scaled=[scale[i]*row['input'][i] for i in range(len(scale))]
+    scaleddata.append({'input':scaled,'result':row['result']})
   return scaleddata
 
-def createcostfunction(algf, data):
+def createcostfunction(algf,data):
   def costf(scale):
-    sdata = rescale(data, scale)
-    return crossvalidate(algf, sdata, trials=10)
+    sdata=rescale(data,scale)
+    return crossvalidate(algf,sdata,trials=20)
   return costf
 
+weightdomain=[(0,10)]*4
+
+def wineset3():
+  rows=wineset1()
+  for row in rows:
+    if random()<0.5:
+      # Wine was bought at a discount store
+      row['result']*=0.6
+  return rows
+
+def probguess(data,vec1,low,high,k=5,weightf=gaussian):
+  dlist=getdistances(data,vec1)
+  nweight=0.0
+  tweight=0.0
+  
+  for i in range(k):
+    dist=dlist[i][0]
+    idx=dlist[i][1]
+    weight=weightf(dist)
+    v=data[idx]['result']
+    
+    # Is this point in the range?
+    if v>=low and v<=high:
+      nweight+=weight
+    tweight+=weight
+  if tweight==0: return 0
+  
+  # The probability is the weights in the range
+  # divided by all the weights
+  return nweight/tweight
+
+from pylab import *
+
+def cumulativegraph(data,vec1,high,k=5,weightf=gaussian):
+  t1=arange(0.0,high,0.1)
+  cprob=array([probguess(data,vec1,0,v,k,weightf) for v in t1])
+  plot(t1,cprob)
+  show()
+
+
+def probabilitygraph(data,vec1,high,k=5,weightf=gaussian,ss=5.0):
+  # Make a range for the prices
+  t1=arange(0.0,high,0.1)
+  
+  # Get the probabilities for the entire range
+  probs=[probguess(data,vec1,v,v+0.1,k,weightf) for v in t1]
+  
+  # Smooth them by adding the gaussian of the nearby probabilites
+  smoothed=[]
+  for i in range(len(probs)):
+    sv=0.0
+    for j in range(0,len(probs)):
+      dist=abs(i-j)*0.1
+      weight=gaussian(dist,sigma=ss)
+      sv+=weight*probs[j]
+    smoothed.append(sv)
+  smoothed=array(smoothed)
+    
+  plot(t1,smoothed)
+  show()
--- chapter08/optimization.py
+++ chapter08/optimization.py
@@ -1,263 +1,168 @@
-# -*- coding: utf-8 -*-
 import time
 import random
-import math 
+import math
 
-people = [
-  ("Syemour", "BOS")
-  , ("Franny", "DAL")
-  , ("Zooey", "CAK")
-  , ("Walt", "MIA")
-  , ("Buddy", "ORD")
-  , ("Les", "OMA")
-]
+people = [('Seymour','BOS'),
+          ('Franny','DAL'),
+          ('Zooey','CAK'),
+          ('Walt','MIA'),
+          ('Buddy','ORD'),
+          ('Les','OMA')]
+# Laguardia
+destination='LGA'
 
-destination = "LGA"
-flights = {}
+flights={}
+# 
+"""
+for line in file('schedule.txt'):
+  origin,dest,depart,arrive,price=line.strip().split(',')
+  flights.setdefault((origin,dest),[])
 
-# 비행 스케줄표를 만든다. 
-#for line in file("schedule.txt"):
-#  origin, dest, depart, arrive, price = line.strip().split(",")
-#  flights.setdefault((origin, dest), [])
-#  flights[(origin, dest)].append((depart, arrive, int(price)))
+  # Add details to the list of possible flights
+  flights[(origin,dest)].append((depart,arrive,int(price)))
+"""
+def getminutes(t):
+  x=time.strptime(t,'%H:%M')
+  return x[3]*60+x[4]
 
-domain = [(0, 9)] * len(people) * 2
-
-# 시간을 분단위로 변환한다. 
-def getMinutes(t):
-  x = time.strptime(t, "%H:%M")
-  return x[3]*60 + x[4]
-
-# 시간에서 시만 가져온다. 
-def getHour(t):
-  x = time.strptime(t, "%H:%M")
-  return x[3]
-
-
-# 리스트를 사람들이 알아볼 수 있는 형식으로 출력한다. 
-def printSchedule(r):
+def printschedule(r):
   for d in range(len(r)/2):
-    name = people[d][0]
-    origin = people[d][1]
-    out = flights[(origin, destination)][r[d*2]]
-    ret = flights[(destination, origin)][r[d*2+1]]
-    print "%10s%10s %5s-%5s $%3s %5s-%5s $%3s" % (name, origin, out[0], out[1], out[2],
-      ret[0], ret[1], ret[2])
+    name=people[d][0]
+    origin=people[d][1]
+    out=flights[(origin,destination)][int(r[d])]
+    ret=flights[(destination,origin)][int(r[d+1])]
+    print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name,origin,
+                                                  out[0],out[1],out[2],
+                                                  ret[0],ret[1],ret[2])
 
-# 비용함수
-def scheduleCost(sol):
-  totalPrice = 0
-  latestArrival = 0
-  earliestDep = 24 * 60
+def schedulecost(sol):
+  totalprice=0
+  latestarrival=0
+  earliestdep=24*60
 
-  # 비행가격을 산출한다. 
   for d in range(len(sol)/2):
-    origin = people[d][1]
-    outbound = flights[(origin, destination)][int(sol[2*d])]
-    returnf = flights[(destination, origin)][int(sol[2*d+1])]
-
-    totalPrice += outbound[2]
-    totalPrice += returnf[2]
-
-    # 가장 늦은 도착시간, 가장 빠른 출발시간을 설정한다. 
-    if latestArrival < getMinutes(outbound[1]): latestArrival = getMinutes(outbound[1])
-    if earliestDep > getMinutes(returnf[0]): earliestDep = getMinutes(returnf[0])
-
-  # 도착시, 출발시 기다리는 시간을 기준으로 페널티를 준다. 
-  totalWait = 0
-  for d in range(len(sol)/2):
-    origin = people[d][1]
-    outbound = flights[(origin, destination)][int(sol[2*d])]
-    returnf = flights[(destination, origin)][int(sol[2*d+1])]
-
-    totalWait += latestArrival - getMinutes(outbound[1])
-    totalWait += getMinutes(returnf[0]) - earliestDep
-
-  # 전체비행시간으로 비용을 산출한다. 
-  totalTime = 0
-  for d in range(len(sol)/2):
-    origin = people[d][1]
-    outbound = flights[(origin, destination)][int(sol[2*d])]
-    returnf = flights[(destination, origin)][int(sol[2*d+1])]
-    totalTime += getMinutes(outbound[1]) - getMinutes(outbound[0])
-    totalTime += getMinutes(returnf[1]) - getMinutes(returnf[0])
-
-  # 8시 이전에 출발하는 비행기에는 20달러 페널티
-  toolEarly = 0
-  for d in range(len(sol)/2):
-    origin = people[d][1]
-    outbound = flights[(origin, destination)][int(sol[2*d])]
-    returnf = flights[(destination, origin)][int(sol[2*d+1])]
-
-    if getHour(outbound[0]) <  8: toolEarly += 1
-    if getHour(returnf[0]) <  8: toolEarly += 1
-
-  # 자동차 렌탈 고려
-  if latestArrival > earliestDep: totalPrice += 50  
-  return totalPrice + totalWait + totalTime * 0.5 + toolEarly * 20
-
-# 무작위 솔루션   
-def randomOptimize(domain, costf):
-  best = 999999999
-  bestr = None 
-  for i in range(10000):
-    # create random solution 
-    r = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
-    cost = costf(r)
+    # Get the inbound and outbound flights
+    origin=people[d][1]
+    outbound=flights[(origin,destination)][int(sol[d])]
+    returnf=flights[(destination,origin)][int(sol[d+1])]
     
-    if cost < best:
-      best = cost 
-      bestr = r
-  return bestr
+    # Total price is the price of all outbound and return flights
+    totalprice+=outbound[2]
+    totalprice+=returnf[2]
+    
+    # Track the latest arrival and earliest departure
+    if latestarrival<getminutes(outbound[1]): latestarrival=getminutes(outbound[1])
+    if earliestdep>getminutes(returnf[0]): earliestdep=getminutes(returnf[0])
+  
+  # Every person must wait at the airport until the latest person arrives.
+  # They also must arrive at the same time and wait for their flights.
+  totalwait=0  
+  for d in range(len(sol)/2):
+    origin=people[d][1]
+    outbound=flights[(origin,destination)][int(sol[d])]
+    returnf=flights[(destination,origin)][int(sol[d+1])]
+    totalwait+=latestarrival-getminutes(outbound[1])
+    totalwait+=getminutes(returnf[0])-earliestdep  
 
-# hill climb, 임의 솔루션을 선택한 뒤, 각 비행편의 앞뒤 비행편으로 바꾸었을 때의 비용을 구해본다.
-def hillClimb(domain, costf):
-  sol = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+  # Does this solution require an extra day of car rental? That'll be $50!
+  if latestarrival>earliestdep: totalprice+=50
+  
+  return totalprice+totalwait
 
-  while 1:
-    neighbors = []
-    for j in range(len(domain)):
-      # 현재 비행편의 앞뒤 비행편을 하나씩 선택한다. 
-      if sol[j] > domain[j][0]:
-        neighbors.append(sol[0:j] + [sol[j]+1] + sol[j+1:])
-      if sol[j] < domain[j][1]:
-        neighbors.append(sol[0:j] + [sol[j]-1] + sol[j+1:])
+def randomoptimize(domain,costf):
+  best=999999999
+  bestr=None
+  for i in range(0,1000):
+    # Create a random solution
+    r=[float(random.randint(domain[i][0],domain[i][1])) 
+       for i in range(len(domain))]
+    
+    # Get the cost
+    cost=costf(r)
+    
+    # Compare it to the best one so far
+    if cost<best:
+      best=cost
+      bestr=r 
+  return r
 
-    current = costf(sol)
-    best = current
-    for j in (range(len(neighbors))):
-      cost = costf(neighbors[j])
-      if cost < best: 
-        best = cost 
-        sol = neighbors[j]
-    # 비용이 기존과 같다면 break        
-    if best == current: break
 
-  return sol
+def annealingoptimize(domain,costf,T=10000.0,cool=0.95,step=1):
+  # Initialize the values randomly
+  vec=[float(random.randint(domain[i][0],domain[i][1])) 
+       for i in range(len(domain))]
+  
+  while T>0.1:
+    # Choose one of the indices
+    i=random.randint(0,len(domain)-1)
 
-# simulated annealing 
-# 특정확률로 나쁜 답을 선택해서 local minimum 을 회피하려고 한다.
-def annealingOptimize(domain, costf, T=10000.0, cool=0.95, step=1):
-  # random initialize 
-  vec = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+    # Choose a direction to change it
+    dir=random.randint(-step,step)
 
-  # 온도는 cool 을 사용해서 감소시킨다. 
-  while T > 0.1:
-    # solution 중 임의의 비행편을 선택한다.
-    sel = random.randint(0, len(domain)-1)
+    # Create a new list with one of the values changed
+    vecb=vec[:]
+    vecb[i]+=dir
+    if vecb[i]<domain[i][0]: vecb[i]=domain[i][0]
+    elif vecb[i]>domain[i][1]: vecb[i]=domain[i][1]
 
-    # 변경할 방향을 선택한다. 
-    dir = random.randint(-step, step)
+    # Calculate the current cost and the new cost
+    ea=costf(vec)
+    eb=costf(vecb)
+    p=pow(math.e,(-eb-ea)/T)
 
-    newVec = vec[:]
-    newVec[sel] += dir 
-    # 범위보정
-    if newVec[sel] < domain[sel][0]: newVec[sel] = domain[sel][0]
-    if newVec[sel] > domain[sel][1]: newVec[sel] = domain[sel][1]
+    print vec,ea
 
-    # 비용을 계산한다.
-    cost = costf(vec)
-    newCost = costf(newVec)
-    print "cost: %f, newCost: %f" % (cost, newCost)
 
-    # 초기에는 T 가 크기 때문에 높은 확률이 나온다. 
-    p = pow(math.e, (-newCost - cost)/T)
+    # Is it better, or does it make the probability
+    # cutoff?
+    if (eb<ea or random.random()<p):
+      vec=vecb      
 
-    # 특정상황에서 솔류션을 교체한다. 
-    if (newCost < cost or random.random() < p): vec = newVec
-
-    T = T * cool
-
+    # Decrease the temperature
+    T=T*cool
   return vec
 
-# 유전자 알고리즘
-# 한 세대마다 mutation crossover 를 거친다.
-def geneticOptimize(domain, costf, popsize=50, step=1, mutprob=0.2, elite=0.2, maxiter=100):
-  # mutation operation - 임의 항공편을 변경한다.
-  def mutate(vec):
-    sel = random.randint(0, len(domain)-1)
-    if random.random() < 0.5 and vec[sel] > domain[sel][0]:
-      return vec[0:sel] + [vec[sel]-step] + vec[sel+1:]
-    elif vec[sel] < domain[sel][1]:
-      return vec[0:sel] + [vec[sel]+step] + vec[sel+1:] 
-    else: return vec
-    
-  # crossover operation - 두 개의 벡터를 교배한다.
-  def crossover(r1, r2):
-    sel = random.randint(0, len(domain)-2)
-    return r1[0:sel] + r2[sel:]
-    
-  # build population 
-  pop = []
-  for i in range(popsize):
-    vec = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
-    pop.append(vec)
+def swarmoptimize(domain,costf,popsize=20,lrate=0.1,maxv=2.0,iters=50):
+  # Initialize individuals
+  # current solutions
+  x=[]
+
+  # best solutions
+  p=[]
+
+  # velocities
+  v=[]
   
-  # 매 세대마다 살아남을 vectors  
-  topElite = int(elite*popsize)
+  for i in range(0,popsize):
+    vec=[float(random.randint(domain[i][0],domain[i][1])) 
+         for i in range(len(domain))]
+    x.append(vec)
+    p.append(vec[:])
+    v.append([0.0 for i in vec])
   
-  bestCost = 9999999999  
-  sameCount = 0
-  for i in range(maxiter):
-    scores = [(costf(v), v) for v in pop]
-    scores.sort() 
-    
-    # cost 가 내려가는지 확인한다.
-    if scores[0][0] < bestCost:
-      bestCost = scores[0][0]
-      sameCount = 0
-    else:
-      sameCount += 1
-    
-    if sameCount == 10:
-      break  
-    
-    ranked = [v for (s, v) in scores]
-    
-    # 다음 세대로 넘어갈 벡터들
-    pop = ranked[0:topElite]
-   
-    # 모자란 세대를 보충한다.  
-    while len(pop) < popsize:
-      # mutation
-      if random.random() < mutprob:
-        # choose random vector from pop
-        sel = random.randint(0, topElite)
-        pop.append(mutate(ranked[sel]))
-      else:
-        # crossover 
-        r1 = random.randint(0, topElite)
-        r2 = random.randint(0, topElite)
-        pop.append(crossover(ranked[r1], ranked[r2]))
-    
-    print scores[0][0]
-    
-  return scores[0][1]
-
-# annealing 방식에 랜덤입력을 넣는다. 
-def randomAnnealingOptimize(domain, costf):
-  best = 999999999
-  bestResult = None 
-
-  for i in range(100):
-    sol = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
-    result = annealingOptimize(domain, costf)
-    newCost = scheduleCost(result)
-    if newCost < best:
-      print("new cost: %f" % newCost)
-      best = newCost
-      bestResult = result
-  return bestResult
-
-
-      
-    
   
-    
+  for ml in range(0,iters):
+    for i in range(0,popsize):
+      # Best solution for this particle
+      if costf(x[i])<costf(p[i]):
+        p[i]=x[i][:]
+      g=i
 
+      # Best solution for any particle
+      for j in range(0,popsize):
+        if costf(p[j])<costf(p[g]): g=j
+      for d in range(len(x[i])):
+        # Update the velocity of this particle
+        v[i][d]+=lrate*(p[i][d]-x[i][d])+lrate*(p[g][d]-x[i][d])
 
+        # constrain velocity to a maximum
+        if v[i][d]>maxv: v[i][d]=maxv
+        elif v[i][d]<-maxv: v[i][d]=-maxv
 
+        # constrain bounds of solutions
+        x[i][d]+=v[i][d]
+        if x[i][d]<domain[d][0]: x[i][d]=domain[d][0]
+        elif x[i][d]>domain[d][1]: x[i][d]=domain[d][1]
 
-
-
-
+    print p[g],costf(p[g])
+  return p[g]
