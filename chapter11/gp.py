6c9d60fad6d9c5a5ff6b13ee89ec3e6cc20e9745
sleeperbus <sleeperbus@gmail.com>
2016-09-13 08:32:05 +0900

    ..

--- chapter11/gp.py
+++ chapter11/gp.py
@@ -160,3 +160,76 @@ def getrankfunction(dataset):
     scores.sort()
     return scores
   return rankfunction
+
+def gridgame(p):
+  # Board size 
+  max = (3, 3)
+
+  # Remember the last move for each player
+  lastmove = [-1, -1]
+
+  # Remember the player's locations 
+  location = [[randint(0, max[0]), randint(0, max[1])]]
+
+  # Put the second player a sufficient distance from the first 
+  location.append([(location[0][0]+2)%4, (location[0][1]+2)%4])
+
+  # Maximum of 50 moves before a tie 
+  for o in range(50):
+    
+    # For each player:
+    for i in range(2):
+      locs = location[i][:] + location[1-i][:]
+      locs.append(lastmove[i])
+      move = p[i].evaluate(locs)%4
+
+      # You lose if you move the same direction twice in a row
+      # 경계 밖으로 나가려고 하면 해당 턴은 무효화
+      if lastmove[i] == move: return 1-i
+      lastmove[i] = move
+      if move == 0:
+        location[i][0] -= 1
+        # 보드 경계에 도달했는가?
+        if location[i][0] < 0: location[i][0] = 0
+      if move == 1:
+        location[i][0] += 1
+        if location[i][0] > max[0]: location[i][0] = max[0]
+      if move == 2:
+        location[i][1] -= 1
+        if location[i][1] < 0: location[i][1] = 0
+      if move == 3:
+        location[i][1] += 1
+        if location[i][1] > max[1]: location[i][1] = max[1]
+
+      if location[i] == location[1-i]: return i
+  return -1
+  
+
+def tournament(pl):
+  # 패배 횟수
+  losses = [0 for p in pl]
+
+  # 모든 선수가 각자 겨룸 
+  for i in range(len(pl)):
+    for j in range(len(pl)):
+      if i == j: continue
+
+      # 누가 승자인지 판단함 
+      winner = gridgame([pl[i], pl[j]])
+
+      # 패배면 2점을, 무승부면 1점을 기록함
+      if winner == 0:
+        losses[j] += 2
+      elif winner == 1:
+        losses[i] += 2
+      elif winner == -1:
+        losses[i] += 1
+        losses[j] += 1
+        pass
+
+  # 정렬해서 결과를 리턴함 
+  z = zip(losses, p1)
+  z.sort()
+  return z
+
+    
